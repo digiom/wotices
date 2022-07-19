e3cebb8988102bc21c1644ac0088e92e2f5191ba
Oleg <109225168+frescoref@users.noreply.github.com>
2022-07-19 19:37:30 +0300

    Renaming to Digiom\Wotices

--- src/Types/InfoNotice.php
+++ src/Types/InfoNotice.php
@@ -1,23 +1,13 @@
-<?php
-/**
- * Namespace
- */
-namespace Digiom\WordPress\Notices\Types;
+<?php namespace Digiom\Wotices\Types;
 
-/**
- * Only WordPress
- */
 defined('ABSPATH') || exit;
 
-/**
- * Dependencies
- */
-use Digiom\WordPress\Notices\Abstracts\NoticeAbstract;
+use Digiom\Wotices\Abstracts\NoticeAbstract;
 
 /**
  * Class InfoNotice
  *
- * @package Digiom\WordPress\Notices\Types
+ * @package Digiom\Wotices\Types
  */
 class InfoNotice extends NoticeAbstract
 {
--- src/Types/SuccessNotice.php
+++ src/Types/SuccessNotice.php
@@ -1,23 +1,13 @@
-<?php
-/**
- * Namespace
- */
-namespace Digiom\WordPress\Notices\Types;
+<?php namespace Digiom\Wotices\Types;
 
-/**
- * Only WordPress
- */
 defined('ABSPATH') || exit;
 
-/**
- * Dependencies
- */
-use Digiom\WordPress\Notices\Abstracts\NoticeAbstract;
+use Digiom\Wotices\Abstracts\NoticeAbstract;
 
 /**
  * Class SuccessNotice
  *
- * @package Digiom\WordPress\Notices\Types
+ * @package Digiom\Wotices\Types
  */
 class SuccessNotice extends NoticeAbstract
 {
